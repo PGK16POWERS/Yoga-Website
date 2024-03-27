f284da5e00e4905f37427f406de488ba551f2401
PGK16POWERS <136324051+PGK16POWERS@users.noreply.github.com>
2024-03-27 23:27:11 +0200

    Still routes

--- yoga/src/main/java/com/yoga/yoga/Routes.java
+++ yoga/src/main/java/com/yoga/yoga/Routes.java
@@ -1,24 +1,24 @@
 package com.yoga.yoga;
 
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.core.io.Resource;
-import org.springframework.web.bind.annotation.RequestParam;
-
+import org.springframework.util.StreamUtils;
 
 @RestController
 public class Routes {
 
     @GetMapping("/")
-    public String Home() {
+    public String Home() throws IOException {
         Resource resource = new ClassPathResource("static/index.html");
 
+        String htmlContent = StreamUtils.copyToString(resource.getInputStream(),StandardCharsets.UTF_8);
 
-
-        return resource;
+        return htmlContent;
     }
     
 
