d914e830b630a5075575885d844314df98c0bbda
PGK16POWERS <136324051+PGK16POWERS@users.noreply.github.com>
2024-03-27 22:14:34 +0200

    Routes Still

--- yoga/src/main/java/com/yoga/yoga/Routes.java
+++ yoga/src/main/java/com/yoga/yoga/Routes.java
@@ -1,7 +1,25 @@
 package com.yoga.yoga;
 
+import java.io.IOException;
+
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.core.io.Resource;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
+@RestController
 public class Routes {
+
+    @GetMapping("/")
+    public String Home() {
+        Resource resource = new ClassPathResource("static/index.html");
+
+
+
+        return resource;
+    }
+    
 
 }
